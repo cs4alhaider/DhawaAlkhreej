fc27b32e4d06a7bcfc459c2630b6f23a8737c658
cs4alhaider <cs.alhaider@gmail.com>
2019-05-17 23:29:34 +0300

    init

---
+++ DhawaAlkhreej/DhawaAlkhreej/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DhawaAlkhreej
+//
+//  Created by Abdullah Alhaider on 17/05/2019.
+//  Copyright © 2019 DhawaAlkhreej. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
