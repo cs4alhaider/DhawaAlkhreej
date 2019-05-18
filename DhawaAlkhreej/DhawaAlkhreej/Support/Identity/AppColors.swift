1b4fade720046f7e2d4c07979de48c976002801d
cs4alhaider <cs.alhaider@gmail.com>
2019-05-18 23:20:48 +0300

    Adding the app colors and storyboards

--- DhawaAlkhreej/DhawaAlkhreej/Support/Identity/AppColors.swift
+++ DhawaAlkhreej/DhawaAlkhreej/Support/Identity/AppColors.swift
@@ -10,8 +10,29 @@ import UIKit
 
 extension UIColor {
     
-    /// Primary color - hex: "0097DB"
+    /// Primary color
     static var primary: UIColor {
-        return #colorLiteral(red: 0, green: 0.5921568627, blue: 0.8588235294, alpha: 1)
+        return UIColor(named: "primary") ?? #colorLiteral(red: 0.9921568627, green: 0.7529411765, blue: 0.3019607843, alpha: 1)
     }
+    
+    /// White background
+    static var whiteBackground: UIColor {
+        return UIColor(named: "whiteBackground") ?? #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    /// White text color
+    static var whiteTextColor: UIColor {
+        return UIColor(named: "whiteTextColor") ?? #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    /// Brown text color
+    static var brownTextColor: UIColor {
+        return UIColor(named: "brownTextColor") ?? #colorLiteral(red: 0.3490196078, green: 0.3333333333, blue: 0.3215686275, alpha: 1)
+    }
+    
+    /// Brown background color
+    static var brownBackground: UIColor {
+        return UIColor(named: "brownBackground") ?? #colorLiteral(red: 0.3490196078, green: 0.3333333333, blue: 0.3215686275, alpha: 1)
+    }
+    
 }
---
+++ DhawaAlkhreej/DhawaAlkhreej/Support/Shared Extensions/UIKit/UIStoryboard+Extension.swift
@@ -0,0 +1,37 @@
+//
+//  UIStoryboard+Extension.swift
+//  DhawaAlkhreej
+//
+//  Created by Abdullah Alhaider on 18/05/2019.
+//  Copyright © 2019 DhawaAlkhreej. All rights reserved.
+//
+
+import Helper4Swift
+
+extension UIStoryboard {
+    
+    /// "Notifications"
+    var notifications: UIStoryboard {
+        return "Notifications".asStoryboard
+    }
+    
+    /// "AboutUs"
+    var AboutUs: UIStoryboard {
+        return "AboutUs".asStoryboard
+    }
+    
+    /// "Calculator"
+    var calculator: UIStoryboard {
+        return "Calculator".asStoryboard
+    }
+    
+    /// "Sections"
+    var sections: UIStoryboard {
+        return "Sections".asStoryboard
+    }
+    
+    /// "SavedItems"
+    var savedItems: UIStoryboard {
+        return "SavedItems".asStoryboard
+    }
+}
