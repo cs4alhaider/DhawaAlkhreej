4b4d2cd7d5b975de29d79354d751905b2914540d
cs4alhaider <cs.alhaider@gmail.com>
2019-05-19 22:39:41 +0300

    Setting up BaseViewProtocol

--- DhawaAlkhreej/DhawaAlkhreej/Support/Base/Protocols/BaseViewProtocol.swift
+++ DhawaAlkhreej/DhawaAlkhreej/Support/Base/Protocols/BaseViewProtocol.swift
@@ -6,8 +6,35 @@
 //  Copyright © 2019 DhawaAlkhreej. All rights reserved.
 //
 
-import Foundation
+import Helper4Swift
 
-protocol BaseViewProtocol {
-    // TODO
+protocol BaseViewProtocol { }
+
+extension BaseViewProtocol where Self: UIViewController {
+    
+    /// Show an alert
+    ///
+    /// - Parameters:
+    ///   - title: title of the alert
+    ///   - buttonTitle: button Title
+    ///   - body: message of the alert
+    func alert(title: String, body: String, buttonTitle: String = "OK", completion: (() -> Void)? = nil) {
+        H4S.showOneActionAlert(vc: Self.init(), title: title, message: body, buttonTitle: buttonTitle, buttonHandler: completion)
+    }
+    
+    /// Showing a loading indecitor
+    ///
+    /// - Parameters:
+    ///   - time: time till the loader dissiper
+    ///   - completion: `(() -> Void)? = nil`
+    func startLoading(for time: TimeInterval = .infinity, completion: (() -> Void)? = nil) {
+        
+    }
+    
+    /// Stopping the loader
+    ///
+    /// - Parameter completion: `(() -> Void)? = nil`
+    func stopLoading(with completion: (() -> Void)? = nil) {
+        
+    }
 }
