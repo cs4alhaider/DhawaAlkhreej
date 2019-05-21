1bcd1af1651f2ee263a53e117418520f30e1cf62
FaroukZeino <33427443+FaroukZeino@users.noreply.github.com>
2019-05-22 02:46:50 +0300

    Customized the appearance of CalculatorVC

--- DhawaAlkhreej/DhawaAlkhreej/Modules/Calculator/CalculatorVC.swift
+++ DhawaAlkhreej/DhawaAlkhreej/Modules/Calculator/CalculatorVC.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class CalculatorVC: BaseViewController {
 
+    @IBOutlet var ThanwiyahLabel: UILabel!
+    @IBOutlet var quodratLabel: UILabel!
+    @IBOutlet var tahsilyLabel: UILabel!
+    @IBOutlet var stepExamLabel: UILabel!
+    @IBOutlet var calculateButton: UIButton!
+    @IBOutlet var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        ThanwiyahLabel.font = Identity.font(.custom(weight: .bold, size: 20))
+        quodratLabel.font = Identity.font(.custom(weight: .bold, size: 20))
+        tahsilyLabel.font = Identity.font(.custom(weight: .bold, size: 20))
+        stepExamLabel.font = Identity.font(.custom(weight: .bold, size: 20))
+        
+        calculateButton.layer.cornerRadius = 10
+        calculateButton.layer.shadowOffset = CGSize.zero
+        calculateButton.layer.shadowOpacity = 0.2
+        calculateButton.titleLabel?.font = Identity.font(.custom(weight: .bold, size: 20))
+        
+        resultLabel.textColor = UIColor.primary
+        resultLabel.font = Identity.font(.custom(weight: .bold, size: 20))
     }
 }
