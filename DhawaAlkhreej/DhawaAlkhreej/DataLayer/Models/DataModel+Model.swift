06283da782fc4f84e27f2990cad33041246f4281
cs4alhaider <cs.alhaider@gmail.com>
2019-06-20 23:44:20 +0300

    Adding DataModel

--- DhawaAlkhreej/DhawaAlkhreej/DataLayer/Models/DataModel+Model.swift
+++ DhawaAlkhreej/DhawaAlkhreej/DataLayer/Models/DataModel+Model.swift
@@ -10,9 +10,34 @@ import Foundation
 
 struct DataModel: Codable {
     
+    static private(set) var current: DataModel?
+    
     let country: CountryModel?
     
     enum CodingKeys: String, CodingKey {
         case country
+    }
+}
+
+extension DataModel {
+    
+    static var universitys: [UniversityModel]? {
+        return DataModel.current?.country?.universitys
+    }
+    
+    static func getUniversitys(completion: @escaping DataResponse<DataModel>) {
+        _getUniversitys { (result) in
+            switch result {
+            case .success(let data):
+                current = data // caching the data in the memory
+                completion(.success(data))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    private static func _getUniversitys(completion: @escaping DataResponse<DataModel>) {
+        Router.University.getUniversitys.request(completion: completion)
     }
 }
